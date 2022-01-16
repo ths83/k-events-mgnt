2f4dff4eeeaeac4009354af0d60bda681b3cae6c
ths83 <ths.perron@gmail.com>
2022-01-15 23:49:16 -0500

    Enable crossOrigin for clients

--- k-events-api/src/main/java/com/ths83/keventsapi/resources/EventResource.java
+++ k-events-api/src/main/java/com/ths83/keventsapi/resources/EventResource.java
@@ -1,12 +1,13 @@
 package com.ths83.keventsapi.resources;
 
-import com.ths83.keventsapi.exceptions.EventAlreadyExistsException;
+import com.ths83.keventsapi.exceptions.EventsException;
 import com.ths83.keventsapi.model.Event;
-import com.ths83.keventsapi.model.EventsPagination;
+import com.ths83.keventsapi.model.EventsResult;
 import com.ths83.keventsapi.services.EventService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,9 +18,12 @@ import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RequiredArgsConstructor
+@CrossOrigin
 @RestController
 @RequestMapping("/events")
 public class EventResource {
+
+	private static final String UNEXPECTED_EXCEPTION_MSG = "An unexpected exception occurred";
 
 	private final EventService service;
 
@@ -37,10 +41,11 @@ public class EventResource {
 		log.info("Calling POST /events endpoint...");
 		try {
 			return service.create(event);
-		} catch (EventAlreadyExistsException e) {
+		} catch (EventsException e) {
+			log.error(e.getMessage());
 			throw new ResponseStatusException(e.getStatus(), e.getMessage());
 		} catch (Exception e) {
-			log.error("An unexpected exception occurred");
+			log.error(UNEXPECTED_EXCEPTION_MSG);
 			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
 		}
 	}
@@ -50,17 +55,17 @@ public class EventResource {
 	 *
 	 * @param page The selected page.
 	 * @param size The desired number of events to retrieve.
-	 * @return {@link EventsPagination}
+	 * @return {@link EventsResult}
 	 * The desired events, the selected page, the total number of elements, the total number of pages.
 	 * @throws ResponseStatusException If any error occurred, a response with status 500 is returned.
 	 */
 	@GetMapping
-	public EventsPagination get(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size) {
+	public EventsResult get(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size) {
 		log.info("Calling GET /events endpoint...");
 		try {
 			return service.getByMostRecentStartDate(page, size);
 		} catch (Exception e) {
-			log.error("An unexpected exception occurred");
+			log.error(UNEXPECTED_EXCEPTION_MSG);
 			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
 		}
 	}
